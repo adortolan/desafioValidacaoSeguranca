8b087b6b5a991cce61ad8479dfeb5612b6832711
adortolan <adortolan@gmail.com>
2024-08-24 12:12:17 -0300

    Implementando Spring Security

---
+++ src/main/java/com/devsuperior/demo/projections/UserDetailsProjections.java
@@ -0,0 +1,8 @@
+package com.devsuperior.demo.projections;
+
+public interface UserDetailsProjections {
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,13 @@ INSERT INTO tb_event(name, date, url, city_id) VALUES ('Feira do Software', '202
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('CCXP', '2021-04-13', 'https://ccxp.com.br', 1);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Congresso Linux', '2021-05-23', 'https://congressolinux.com.br', 2);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Semana Spring React', '2021-05-03', 'https://devsuperior.com.br', 3);
+
+INSERT INTO tb_user (name, email, password) VALUES ('Ana', 'ana@gmail.com', '$2a$10$q1O8hyc5Kq8HvxUOUUos4eiifjmrvRfewEA3SCm8j1bb7YVvsdcrm');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob', 'bob@gmail.com', '$2a$10$q1O8hyc5Kq8HvxUOUUos4eiifjmrvRfewEA3SCm8j1bb7YVvsdcrm');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
