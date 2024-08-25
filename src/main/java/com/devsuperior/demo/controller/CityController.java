a6c04b3ff1a8550b1eb34f1942ff44358f586eba
adortolan <adortolan@gmail.com>
2024-08-25 11:20:54 -0300

    Implementando TDD na Entidade City

---
+++ src/main/java/com/devsuperior/demo/controller/CityController.java
@@ -0,0 +1,43 @@
+package com.devsuperior.demo.controller;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@Controller
+@RequestMapping("/cities")
+public class CityController {
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@Validated @RequestBody CityDTO dto) {
+        CityDTO newDto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newDto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(newDto);
+    }
+}
